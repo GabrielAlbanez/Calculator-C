9c8633021db9f11294de3cb57f3f5a26d3b4d779
GabrielAlbanez <gabriel.g.albanez@gmail.com>
2024-04-04 12:24:06 -0300

    .

--- calculator.c
+++ calculator.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
+// assim que faz para listar os intens de uma matriz
+// passa ela com o valor de quanntas colunas tem e o tamanho da array
+void listeningItems(float contas[][3], int sizeLengthList)
+int i,j
+{
+    for ( i = 0; i < sizeLengthList; i++)
+
+    {
+        printf("indice %d\n",i);
+        for ( j = 0; j < 3; j++)
+        {
+            printf("%.2f ", contas[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main(void)
 {
     float numero1[50];
@@ -50,22 +67,24 @@ int main(void)
             continue;
         }
 
-        printf("Deseja realizar mais operacoes? Digite '1' para continuar ou '2' para ver suas conntas: ");
+        printf("Deseja realizar mais operacoes? Digite '1' para continuar ou '2' para ver suas contas: ");
         scanf("%d", &limit);
 
         i++;
     }
 
+    printf("Minnha calculadora\n");
     printf("\nResultados das operacoes:\n");
     printf("-------------------------------------------------\n");
 
     for (int l = 0; l < i; l++)
     {
         printf("conta %d\n", l);
-        printf("numero 1 : %2f | operador : %s | numero 2 : %2f  | total : %2f  |\n", Contas[l][0], operador[l], Contas[l][1], Contas[l][2]);
+        printf("numero 1 : %.2f | operador : %s | numero 2 : %.2f  | total : %.2f  |\n", Contas[l][0], operador[l], Contas[l][1], Contas[l][2]);
         printf("-------------------------------------------------\n");
     }
-   
 
+    // aq a gente passa o valor
+    // listeningItems(Contas, i);
     return 0;
 }
